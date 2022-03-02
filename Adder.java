7f2f24cd7101f28ba542113be943445f7efe9f4e
with-yahya-farooqui <84168130+with-yahya-farooqui@users.noreply.github.com>
2022-03-02 10:04:38 +0530

    Create Adder.java

---
+++ Adder.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Adder 
+{
+    int[] array;int variable;
+    Adder()
+    {
+        array=new int[0];
+        variable=0;
+    }
+    void getdata()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter length of array");
+        int n=sc.nextInt();
+        System.out.println("Enter elements of array.");
+        for(int i=0;i<n;i++) array[i]=sc.nextInt();
+        System.out.println("Enter target sum.");
+        variable=sc.nextInt();
+    }
+    int[] numsum()
+    {
+        for(int i =0;i<array.length;i++)
+        {
+            for(int j=0;j<array.length;j++)
+            {
+                if(array[i]+array[j]==variable&&i!=j) {int[] arr=new int[0];arr[0]=array[i];arr[1]=array[j];return arr;}
+            }
+        }
+        return new int[0];
+    }
+}
+class Main{
+    public static void main(String args[])
+    {
+        Adder a=new Adder();
+        a.getdata();
+        int[] arr=a.numsum();
+        if(arr.length==0){System.out.println("Null Array");return;}
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
